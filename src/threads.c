d8d95f0cf519cbedfb5ad8f45f293c348a622981
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-08-07 20:48:29 +0200

    Update threads.c

--- src/threads.c
+++ src/threads.c
@@ -6,20 +6,52 @@
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/13 18:32:11 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/06/16 12:58:18 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/08/07 19:15:33 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-int	ft_threads(t_data *data)
+static int	ft_start_philo(t_data *data, t_philo *philos, t_arg arg, int pos)
 {
-	printf("yes\n");	//rm
-	printf("nb_philo: %d\n", data->nb_philo);	//rm
-	printf("time_to_die: %d\n", data->time_to_die);	//rm
-	printf("time_to_eat: %d\n", data->time_to_eat);	//rm
-	printf("time_to_sleep: %d\n", data->time_to_sleep);	//rm
-	printf("max_eat_count: %d\n", data->max_eat_count);	//rm
-	return (1); // If everything goes right
-	// return (0); If something goes wrong
+	pthread_t	thread;
+
+	data->arg = arg;
+	data->philos = philos;
+	data->pos = pos;
+	data->philos[pos].last_meal = get_time();
+	if (pthread_create(&thread, NULL, &ft_start_routine, data))
+		return (1);
+	pthread_detach(thread);
+	return (0);
 }
+
+static int	ft_start(t_data *data, t_philo *philos, t_arg arg)
+{
+	int	i;
+
+	i = 0;
+	while (i < arg.nb_philos)
+	{
+		if (ft_start_philo(data, philos, arg, i))
+			return (1);
+		i += 2;
+		if (i % 2 == 0 && i >= arg.nb_philos)
+			i = 1;
+	}
+	return (0);
+}
+
+int ft_threads(t_data *data)
+{
+	ft_start(data, data->philos, data->arg);
+	printf("TEST 1:\n"); //RM
+	check_meals(data->philos, data->arg);
+	printf("TEST 2:\n"); //RM
+	ft_destroy_mutex(data->philos, data->arg);
+	printf("TEST 3:\n"); //RM
+	ft_wait(1);
+	printf("TEST 4:\n"); //RM
+	return (1);
+}
+
