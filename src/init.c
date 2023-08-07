bfa960bfd6a852b113bbcaca4eeed7dddaf6281a
Jovanka Metzger <117259075+JoviMetzger@users.noreply.github.com>
2023-08-07 20:47:14 +0200

    Update init.c

--- src/init.c
+++ src/init.c
@@ -6,63 +6,44 @@
 /*   By: jmetzger <jmetzger@student.codam.n>          +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/13 09:16:54 by jmetzger      #+#    #+#                 */
-/*   Updated: 2023/06/14 09:18:48 by jmetzger      ########   odam.nl         */
+/*   Updated: 2023/08/07 20:29:03 by jmetzger      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-/* ft_init_mutex();
-*	This function initializes mutexes for each fork, as well as 
-*	two additional mutexes (meal_check and writing) in the t_data structure. 
-*	It returns 1 if there is an error during initialization, and 0 otherwise.
-*		- The pthread_mutex_init() initialize the mutex corresponding to 
-*		  the i-th fork, meal_check, writing. 
-*		- The NULL argument is passed as the second argument, 
-*		  indicating default attributes for the mutex.
-*		- If the pthread_mutex_init function returns a non-zero value, 
-*		  it means an error occurred during initialization. 
-*		  In such a case, the function returns 1 to indicate the error.
-*/
-static int	ft_init_mutex(t_data *data)
+static t_arg	ft_init_arg(int argc, char **argv)
 {
-	int	i;
+	t_arg	arg;
 
-	i = 0;
-	while (i < data->nb_philo)
-	{
-		if (pthread_mutex_init(&(data->forks[i]), NULL))
-			return (1);
-		i++;
-	}
-	if (pthread_mutex_init((&data->meal_check), NULL))
-		return (1);
-	if (pthread_mutex_init((&data->writing), NULL))
-		return (1);
-	return (0);
+	arg.nb_philos = ft_atoi(argv[1]);
+	arg.time_to_die = ft_atoi(argv[2]);
+	arg.time_to_eat = ft_atoi(argv[3]);
+	arg.time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		arg.nb_of_times_each_philo_must_eat = ft_atoi(argv[5]);
+	else
+		arg.nb_of_times_each_philo_must_eat = 0;
+	return (arg);
 }
 
-/* ft_init_philos();
-*	The function initializes the philosopher structures in the t_data structure. 
-*	It assigns values to various fields of each philosopher.
-*	It returns 0 to indicate successful initialization.
-*/
-static int	ft_init_philos(t_data *data)
+static t_philo	*init_philos_list(t_arg arg)
 {
-	int	i;
+	int		i;
+	t_philo	*philos;
 
-	i = 0;
-	while (i < data->nb_philo)
+	philos = ft_calloc(arg.nb_philos, sizeof(t_philo));
+	if (!philos)
+		return (NULL);
+	i = -1;
+	while (++i < arg.nb_philos)
 	{
-		data->philos[i].pos = i;
-		data->philos[i].ate_times = 0;
-		data->philos[i].left_fork_pos = i;
-		data->philos[i].right_fork_pos = (i + 1) % data->nb_philo;
-		data->philos[i].last_meal = 0;
-		data->philos[i].data = data;
-		i++;
+		pthread_mutex_init(&(philos[i].fork), NULL);
+		pthread_mutex_init(&(philos[i].update), NULL);
+		philos[i].state = sleeping;
+		philos->last_meal = 0;
 	}
-	return (0);
+	return (philos);
 }
 
 /* ft_init();
@@ -72,23 +53,26 @@ static int	ft_init_philos(t_data *data)
 *	It returns 0 if there is an error during initialization, and 1 otherwise.
 */
 int	ft_init(t_data *data, int argc, char **argv)
-{
-	data->nb_philo = ft_atoi(argv[1]);
-	data->time_to_die = ft_atoi(argv[2]);
-	data->time_to_eat = ft_atoi(argv[3]);
-	data->time_to_sleep = ft_atoi(argv[4]);
-	if (argc == 6)
-		data->max_eat_count = ft_atoi(argv[5]);
-	else
-		data->max_eat_count = 0;
-	data->stop_condition = 0;
-	data->all_ate = 0;
-	data->forks = (pthread_mutex_t *)ft_calloc(sizeof(pthread_mutex_t), \
-			data->nb_philo);
-	data->philos = (t_philo *)ft_calloc(sizeof(t_philo), data->nb_philo);
-	if (ft_init_philos(data))
+{	
+	data->arg = ft_init_arg(argc, argv);
+	printf("INIT - arg:\n");
+	print_args(data->arg);
+	data->philos = init_philos_list(data->arg);
+	if (!data->philos)
+	{
+		free(data);
 		return (0);
-	if (ft_init_mutex(data))
-		return (0);
+	}
+	printf("INIT - philo:\n");
 	return (1);
+}
+
+
+void	print_args(t_arg arg)
+{
+	printf("%d ", arg.nb_philos);
+	printf("%d ", arg.time_to_die);
+	printf("%d ", arg.time_to_eat);
+	printf("%d ", arg.time_to_sleep);
+	printf("%d\n", arg.nb_of_times_each_philo_must_eat);
 }
